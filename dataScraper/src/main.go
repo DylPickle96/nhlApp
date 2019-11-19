72d6036cab4160cc9a2723f53dddaaff85ad126c
dsmith <dsmith@adacado.com>
2019-11-19 08:09:21 -0800

    Code comments, renamed the teamRecord struct fields to be exported so it can be used with the JSON.Marshal function. Added a function to write JSON to a file. Only testing for now will have to write daily records to a file for each day we scraper

--- dataScraper/src/main.go
+++ dataScraper/src/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
-	"net/http"
+	"log"
+	"os"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -44,44 +44,49 @@ var nhlCities = []string{
 	"Los Angeles",
 }
 
+type dailyRecord struct {
+	TeamRecords []teamRecord `json:"teamRecords"`
+}
 type teamRecord struct {
-	teamName         string
-	wins             string
-	loses            string
-	overtime         string
-	ROW              string
-	points           string
-	goalsFor         string
-	goalsAgainst     string
-	home             string
-	away             string
-	divisionRecord   string
-	conferenceRecord string
-	icf              string
+	TeamName         string `json:"teamName"`
+	Wins             string `json:"wins"`
+	Loses            string `json:"loses"`
+	Overtime         string `json:"overtime"`
+	ROW              string `json:"ROW"`
+	Points           string `json:"points"`
+	GoalsFor         string `json:"goalsFor"`
+	GoalsAgainst     string `json:"goalsAgainst"`
+	Home             string `json:"home"`
+	Away             string `json:"away"`
+	DivisionRecord   string `json:"divisionRecord"`
+	ConferenceRecord string `json:"conferenceRecord"`
+	ICF              string `json:"ICF"`
 }
 
 func main() {
-	resp, err := http.Get("http://www.shrpsports.com/nhl/stand.php?link=Y&season=2020&divcnf=div&month=Nov&date=6")
-	if err != nil {
-		panic(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	// HTMLFile, err := os.Open("dylan.html")
+	// resp, err := http.Get("http://www.shrpsports.com/nhl/stand.php?link=Y&season=2020&divcnf=div&month=Nov&date=6")
 	// if err != nil {
 	// 	panic(err)
 	// }
-	fileParser(body)
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	HTMLFile, err := os.Open("dylan.html")
+	if err != nil {
+		panic(err)
+	}
+	fileParser(HTMLFile)
 }
 
-func fileParser(HTMLFile []byte) {
-	reader := bytes.NewReader(HTMLFile)
-	tokenizer := html.NewTokenizer(reader)
+// fileParser main function to handle the parsing of the HTML file which are trying to scrape
+func fileParser(HTMLFile *os.File) {
+	// reader := bytes.NewReader(HTMLFile)
+	tokenizer := html.NewTokenizer(HTMLFile)
 	TDCount := 0
 	var tr teamRecord
 	var teamRecords []teamRecord
+	var dailyRecord dailyRecord
 	for {
 		nextToken := tokenizer.Next()
 		if nextToken == html.StartTagToken {
@@ -96,7 +101,7 @@ func fileParser(HTMLFile []byte) {
 						TDCount = 0
 						continue
 					}
-					tr.teamName = value
+					tr.TeamName = value
 				}
 			}
 			// the table digit is the most important thing we care about this contains the data we are after
@@ -105,12 +110,10 @@ func fileParser(HTMLFile []byte) {
 				var textValue string
 				if innerToken == html.TextToken {
 					textValue = strings.TrimSpace((string)(tokenizer.Text()))
-					// fmt.Printf("count: %d. Value: %s\n", TDCount, textValue)
 				}
 				tr = parsePostion(&TDCount, &textValue, tr)
 				// append if this has been reset. Means we have reached the end of the row
 				if TDCount == 10 {
-					// fmt.Printf("count %d. teamRecord: %+v\n", TDCount, tr)
 					teamRecords = append(teamRecords, tr)
 					TDCount = 0
 					continue
@@ -134,42 +137,118 @@ func fileParser(HTMLFile []byte) {
 		}
 	}
 	teamRecords = validateData(teamRecords)
+	// validate data function almost works the way it should. It ends up ignoring one of the two bad records at the beginnning of the slice
+	// so the bad solution right now is call removeRecords here as well on the zeroth index
+	// this poor practice but the more important thing here is that it works
 	teamRecords = removeRecord(teamRecords, 0)
-	fmt.Println(len(teamRecords))
-	for _, tr := range teamRecords {
-		fmt.Printf("%+v\n", tr)
-	}
+	dailyRecord.TeamRecords = teamRecords
+	writeJSONFile(dailyRecord)
 }
 
+// parsePostion parse position assigns the value for that given position to the coorsponding teamRecord value
 func parsePostion(TDCount *int, value *string, teamRecord teamRecord) teamRecord {
 	switch count := *TDCount; count {
 	case 1:
 		winsLoses := strings.Split(*value, "-")
 		if len(winsLoses) == 3 {
-			teamRecord.wins = winsLoses[0]
-			teamRecord.loses = winsLoses[1]
-			teamRecord.overtime = winsLoses[2]
+			teamRecord.Wins = winsLoses[0]
+			teamRecord.Loses = winsLoses[1]
+			teamRecord.Overtime = winsLoses[2]
 		}
 	case 2:
 		teamRecord.ROW = *value
 	case 3:
-		teamRecord.points = *value
+		teamRecord.Points = *value
 	case 4:
-		teamRecord.goalsFor = *value
+		teamRecord.GoalsFor = *value
 	case 5:
-		teamRecord.goalsAgainst = *value
+		teamRecord.GoalsAgainst = *value
 	case 6:
-		teamRecord.home = *value
+		teamRecord.Home = *value
 	case 7:
-		teamRecord.away = *value
+		teamRecord.Away = *value
 	case 8:
-		teamRecord.divisionRecord = *value
+		teamRecord.DivisionRecord = *value
 	case 9:
-		teamRecord.conferenceRecord = *value
+		teamRecord.ConferenceRecord = *value
 	case 10:
-		teamRecord.icf = *value
+		teamRecord.ICF = *value
 	}
 	return teamRecord
+}
+
+// validateData checks if any of the values in a record are an empty string. This process can generate some blank records so we use this function to remove them
+func validateData(teamRecords []teamRecord) []teamRecord {
+	for i, tr := range teamRecords {
+		if tr.Wins == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.Loses == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.Overtime == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.ROW == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.Points == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.GoalsFor == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.GoalsAgainst == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.Home == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.Away == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.DivisionRecord == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.ConferenceRecord == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+		if tr.ICF == "" {
+			teamRecords = removeRecord(teamRecords, i)
+			continue
+		}
+	}
+	return teamRecords
+}
+
+func writeJSONFile(dailyRecord dailyRecord) {
+	JSONBytes, err := json.MarshalIndent(dailyRecord, "", "  ")
+	if err != nil {
+		log.Printf("WARNING: could not marshal slice of teamRecord into JSON. Error: %v", err)
+	}
+	err = ioutil.WriteFile("JSON/test.json", JSONBytes, 0755)
+	if err != nil {
+		log.Printf("WARNING: could not write JSON to file. Error: %v", err)
+	}
+}
+
+// removeRecord removes a record from the slice of team records that are scraped from the website
+func removeRecord(teamRecords []teamRecord, index int) []teamRecord {
+	// fmt.Println("DEBUG: index: ", index)
+	// fmt.Printf("DEBUG: teamRecord: %+v\n", teamRecords[index])
+	teamRecords = append(teamRecords[:index], teamRecords[index+1:]...)
+	return teamRecords
 }
 
 // includesCity checks if the passed in value is contained with the list of cities
@@ -180,67 +259,4 @@ func includesCity(value string) bool {
 		}
 	}
 	return false
-}
-
-// validateData
-func validateData(teamRecords []teamRecord) []teamRecord {
-	for i, tr := range teamRecords {
-		if tr.wins == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.loses == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.overtime == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.ROW == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.points == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.goalsFor == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.goalsAgainst == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.home == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.away == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.divisionRecord == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.conferenceRecord == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-		if tr.icf == "" {
-			teamRecords = removeRecord(teamRecords, i)
-			continue
-		}
-	}
-	return teamRecords
-}
-
-// removeRecord
-func removeRecord(teamRecords []teamRecord, index int) []teamRecord {
-	// fmt.Println("DEBUG: index: ", index)
-	// fmt.Printf("DEBUG: teamRecord: %+v\n", teamRecords[index])
-	teamRecords = append(teamRecords[:index], teamRecords[index+1:]...)
-	return teamRecords
 }
